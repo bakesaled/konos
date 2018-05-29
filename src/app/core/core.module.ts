ea6275738e4ee1b741803fdc561120d3779394af
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2018-05-29 11:21:30 -0600

    adding jwt interceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthGuard } from './auth.guard';
+import { JwtInterceptor } from './jwt.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [],
-  providers: [AuthGuard]
+  providers: [
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
+  ]
 })
 export class CoreModule {}
