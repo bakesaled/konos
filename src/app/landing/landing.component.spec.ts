7328fd689fcab0d88bf400b21e0e1e1f6a74aab0
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2018-05-30 10:23:51 -0600

    fixing tests

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '../core/user.service';
+import { localStorageMock } from '../core/mocks/local-storage.mock';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,11 +12,19 @@ describe('LandingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LandingComponent]
+      declarations: [LandingComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [UserService]
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    spyOn(localStorage, 'getItem').and.callFake(localStorageMock.getItem);
+    spyOn(localStorage, 'setItem').and.callFake(localStorageMock.setItem);
+    spyOn(localStorage, 'removeItem').and.callFake(localStorageMock.removeItem);
+    spyOn(localStorage, 'clear').and.callFake(localStorageMock.clear);
+    localStorage.setItem('currentUser', JSON.stringify({}));
+
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../core/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +12,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [AuthService]
     }).compileComponents();
   }));
 
